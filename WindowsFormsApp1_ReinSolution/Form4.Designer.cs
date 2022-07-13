3d6b95d2f693e8cd3795794b9245a0a94e6209b9
Smita3434 <sayalig94@gmail.com>
2022-07-13 16:19:00 +0530

    new updates

--- WindowsFormsApp1_ReinSolution/Form4.Designer.cs
+++ WindowsFormsApp1_ReinSolution/Form4.Designer.cs
@@ -37,9 +37,9 @@ namespace WindowsFormsApp1_ReinSolution
             this.button6 = new System.Windows.Forms.Button();
             this.button7 = new System.Windows.Forms.Button();
             this.button8 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtPrice = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
@@ -47,61 +47,67 @@ namespace WindowsFormsApp1_ReinSolution
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(212, 165);
+            this.button1.Location = new System.Drawing.Point(222, 144);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
             this.button1.Text = "Binary Write";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(408, 165);
+            this.button2.Location = new System.Drawing.Point(364, 144);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 1;
             this.button2.Text = "Binary Read";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(212, 204);
+            this.button3.Location = new System.Drawing.Point(222, 193);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 2;
             this.button3.Text = "XML Write";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(408, 204);
+            this.button4.Location = new System.Drawing.Point(364, 193);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(75, 23);
             this.button4.TabIndex = 3;
             this.button4.Text = "XML Read";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(212, 242);
+            this.button5.Location = new System.Drawing.Point(222, 243);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(75, 23);
             this.button5.TabIndex = 4;
             this.button5.Text = "SOAP Write";
             this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(408, 242);
+            this.button6.Location = new System.Drawing.Point(364, 243);
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(75, 23);
             this.button6.TabIndex = 5;
             this.button6.Text = "SOAP Read";
             this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // button7
             // 
-            this.button7.Location = new System.Drawing.Point(212, 281);
+            this.button7.Location = new System.Drawing.Point(222, 293);
             this.button7.Name = "button7";
             this.button7.Size = new System.Drawing.Size(75, 23);
             this.button7.TabIndex = 6;
@@ -111,7 +117,7 @@ namespace WindowsFormsApp1_ReinSolution
             // 
             // button8
             // 
-            this.button8.Location = new System.Drawing.Point(408, 281);
+            this.button8.Location = new System.Drawing.Point(364, 293);
             this.button8.Name = "button8";
             this.button8.Size = new System.Drawing.Size(75, 23);
             this.button8.TabIndex = 7;
@@ -119,55 +125,56 @@ namespace WindowsFormsApp1_ReinSolution
             this.button8.UseVisualStyleBackColor = true;
             this.button8.Click += new System.EventHandler(this.button8_Click);
             // 
-            // textBox1
+            // txtId
             // 
-            this.textBox1.Location = new System.Drawing.Point(349, 47);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(199, 20);
-            this.textBox1.TabIndex = 8;
+            this.txtId.Location = new System.Drawing.Point(364, 47);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(100, 20);
+            this.txtId.TabIndex = 8;
+            this.txtId.Text = "     ";
             // 
-            // textBox2
+            // txtName
             // 
-            this.textBox2.Location = new System.Drawing.Point(349, 83);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(199, 20);
-            this.textBox2.TabIndex = 9;
+            this.txtName.Location = new System.Drawing.Point(364, 73);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 9;
             // 
-            // textBox3
+            // txtPrice
             // 
-            this.textBox3.Location = new System.Drawing.Point(349, 126);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(199, 20);
-            this.textBox3.TabIndex = 10;
+            this.txtPrice.Location = new System.Drawing.Point(364, 111);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(100, 20);
+            this.txtPrice.TabIndex = 10;
+            this.txtPrice.TextChanged += new System.EventHandler(this.txtPrice_TextChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(209, 47);
+            this.label1.Location = new System.Drawing.Point(232, 47);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.Size = new System.Drawing.Size(58, 13);
             this.label1.TabIndex = 11;
-            this.label1.Text = "ProductId";
+            this.label1.Text = "Product ID";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(209, 90);
+            this.label2.Location = new System.Drawing.Point(222, 80);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(72, 13);
+            this.label2.Size = new System.Drawing.Size(75, 13);
             this.label2.TabIndex = 12;
-            this.label2.Text = "ProductName";
+            this.label2.Text = "Product Name";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(209, 133);
+            this.label3.Location = new System.Drawing.Point(240, 118);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(31, 13);
             this.label3.TabIndex = 13;
             this.label3.Text = "Price";
-            this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // Form4
             // 
@@ -177,9 +184,9 @@ namespace WindowsFormsApp1_ReinSolution
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtPrice);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtId);
             this.Controls.Add(this.button8);
             this.Controls.Add(this.button7);
             this.Controls.Add(this.button6);
@@ -205,9 +212,9 @@ namespace WindowsFormsApp1_ReinSolution
         private System.Windows.Forms.Button button6;
         private System.Windows.Forms.Button button7;
         private System.Windows.Forms.Button button8;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtPrice;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
--- WindowsFormsApp1_ReinSolution/Form4.cs
+++ WindowsFormsApp1_ReinSolution/Form4.cs
@@ -6,23 +6,74 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization.Formatters.Soap;
 using System.Windows.Forms;
+using System.Xml.Serialization;
+using System.Text.Json;
 
 namespace WindowsFormsApp1_ReinSolution
 {
+    
     public partial class Form4 : Form
     {
+        FileStream fs;
         public Form4()
         {
             InitializeComponent();
         }
 
-        private void button8_Click(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e)
         {
+            try
+            {
+                Product prod = new Product();
+                prod.ID = Convert.ToInt32(txtId.Text);
+                prod.Name = txtName.Text;
+                prod.Price = Convert.ToInt32(txtPrice.Text);
+                FileStream fs = new FileStream(@"D:\Product", FileMode.Create, FileAccess.Write);
+                BinaryFormatter bf = new BinaryFormatter();
+                bf.Serialize(fs, prod);
+             
+                MessageBox.Show("File Created");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                fs.Close();
+            }
 
         }
 
-        private void button7_Click(object sender, EventArgs e)
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Product prod = new Product();
+                FileStream fs = new FileStream(@"G:\thinkQu\Product", FileMode.Open, FileAccess.Read);
+                BinaryFormatter bf = new BinaryFormatter();
+                prod = (Product)bf.Deserialize(fs);
+                txtId.Text = prod.ID.ToString();
+                txtName.Text = prod.Name;
+                txtPrice.Text = prod.Price.ToString();
+               
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                fs.Close();
+            }
+
+        }
+
+        private void txtPrice_TextChanged(object sender, EventArgs e)
         {
 
         }
@@ -32,9 +83,144 @@ namespace WindowsFormsApp1_ReinSolution
 
         }
 
-        private void label3_Click(object sender, EventArgs e)
+        private void button3_Click(object sender, EventArgs e)
         {
+            try
+            {
+                Product prod = new Product();
+                prod.ID = Convert.ToInt32(txtId.Text);
+                prod.Name = txtName.Text;
+                prod.Price = Convert.ToInt32(txtPrice.Text);
+                fs = new FileStream(@"G:thinkQu\ProductXml", FileMode.Create, FileAccess.Write);
+                XmlSerializer xs = new XmlSerializer(typeof(Product));
+                xs.Serialize(fs, prod);
+                MessageBox.Show("XML File Created");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                fs.Close();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Product prod = new Product();
+                fs = new FileStream(@"G:thinkQu\ProductXml", FileMode.Open, FileAccess.Read);
+                XmlSerializer xs = new XmlSerializer(typeof(Product));
+                prod = (Product)xs.Deserialize(fs);
+                txtId.Text = prod.ID.ToString();
+                txtName.Text = prod.Name;
+                txtPrice.Text = prod.Price.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                fs.Close();
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Product prod = new Product();
+                prod.ID = Convert.ToInt32(txtId.Text);
+                prod.Name = txtName.Text;
+                prod.Price = Convert.ToInt32(txtPrice.Text);
+                fs = new FileStream(@"G:thinkQu\ProductSOAP", FileMode.Create, FileAccess.Write);
+                SoapFormatter sf = new SoapFormatter();
+                sf.Serialize(fs, prod);
+                MessageBox.Show("Soap File Created");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                fs.Close();
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Product prod = new Product();
+                fs = new FileStream(@"G:thinkQu\ProductXml", FileMode.Open, FileAccess.Read);
+                SoapFormatter sf = new SoapFormatter();
+                prod = (Product)sf.Deserialize(fs);
+                txtId.Text = prod.ID.ToString();
+                txtName.Text = prod.Name;
+                txtPrice.Text = prod.Price.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                fs.Close();
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Product prod = new Product();
+                prod.ID = Convert.ToInt32(txtId.Text);
+                prod.Name = txtName.Text;
+                prod.Price = Convert.ToInt32(txtPrice.Text);
+                fs = new FileStream(@"G:thinkQu\ProductJson", FileMode.Create, FileAccess.Write);
+                JsonSerializer.Serialize(fs, prod);
+                MessageBox.Show("json File Created");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                fs.Close();
+            }
+
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Product prod = new Product();
+                fs = new FileStream(@"G:thinkQu\ProductJson", FileMode.Open, FileAccess.Read);
+                prod = JsonSerializer.Deserialize<Product>(fs);
+                txtId.Text = prod.ID.ToString();
+                txtName.Text = prod.Name;
+                txtPrice.Text = prod.Price.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                fs.Close();
+            }
 
         }
     }
 }
+
+
+
+
